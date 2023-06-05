de16aa5500754975a2df12791d75f159b5340e91
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2023-06-05 09:04:43 +0200

    Find the Runner-Up Score!

---
+++ Find the Runner-Up Score!.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    final_list = []
+    for num in arr:
+        if num not in final_list:
+            final_list.append(num)
+    final_list.sort()
+
+    print(final_list[-2])
