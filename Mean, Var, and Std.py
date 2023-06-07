bcd66255aa55dd740c92af1b86152e9940b71742
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2023-06-07 15:27:16 +0200

    Mean, Var, and Std

--- Mean, Var, and Std.py
+++ Mean, Var, and Std.py
@@ -1,7 +1,6 @@
 import numpy
-N, M = map(int, input().split())
-_ = numpy.array([input().split() for i in range(N)], int)
-numpy.set_printoptions(legacy='1.13')
-print(numpy.mean(_, axis=1))
-print(numpy.var(_, axis=0))
-print(numpy.std(_, axis=None))
+N,M = map(int, input().split(" "))
+A = numpy.array([input().split() for _ in range(N)],int)
+print(numpy.mean(A, axis = 1))
+print(numpy.var(A, axis = 0))
+print(round(numpy.std(A, axis = None),11))
