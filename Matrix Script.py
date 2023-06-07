45c9694ddc68fd279a688af3b7f30754916f1293
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2023-06-07 15:14:45 +0200

    Matrix Script

---
+++ Matrix Script.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+count = 0
+decode = ''
+val = list(map(int, input().split()))
+input_val = [input() for x in range(val[0])]
+while count != val[1]:
+    for y in input_val:
+        decode += y[count]
+    count += 1
+print(re.sub(r'(?<=([a-zA-Z0-9]))[\s$#%&]+(?=[a-zA-Z0-9])',' ', decode))
