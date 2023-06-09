0f216b5187498ef3e9cd5ce66f67b8fd38d5516e
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2023-06-09 14:59:11 +0200

    Calendar Module

--- Calendar Module.py
+++ Calendar Module.py
@@ -1,22 +1,4 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import calendar
-m,d,y=map(int,input().split())
-print(calendar.day_name[calendar.weekday(y,m,d)].upper())
-####################another method#########################
-import calendar
-m,d,y=map(int,input().split())
-c = calendar.weekday(y, m, d)
-if c == 0:
-    print("MONDAY")
-elif c == 1:
-    print("TUESDAY")
-elif c == 2:
-    print("WEDNESDAY")
-elif c==3:
-    print("THURSDAY")
-elif c==4:
-    print("FRIDAY")
-elif c== 5:
-    print("SATURDAY")
-elif c==6:
-    print("SUNDAY")
+n1,n2,n3=map(int,input().split())
+print((calendar.day_name[calendar.weekday(n3,n1,n2)]).upper())
