0111cff5da947637bf1661c67acbae6b3548a5cb
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2023-06-09 14:38:29 +0200

    DefaultDict Tutorial

---
+++ DefaultDict Tutorial.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import defaultdict
+n,m=[int(x) for x in input().split(' ')]
+lis=[]
+d=defaultdict(list)
+for i in range(1,n+1):
+    d[input()].append(i)
+for _ in range(m):
+    lis.append(input())
+for x in lis:
+    if x in d:
+        print(' '.join(map(str,d[x])))
+    else:
+        print(-1)
